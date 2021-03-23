ae6afc4e2c1c4aa833998402c8e9997d36d66473
Wisinewski <g_gomees@hotmail.com>
2021-03-23 19:09:04 -0300

    test: ensure DbAddAccount call AddAccountRepository with correct values

--- backend/src/data/test/mock-criptography.ts
+++ backend/src/data/test/mock-criptography.ts
@@ -2,7 +2,7 @@ import { Hasher } from './../protocols/criptography/hasher';
 
 export class HasherSpy implements Hasher {
   value: string
-  result: string = 'hashed_value'
+  result: string = 'hashed_password'
   async hash (value: string): Promise<string> {
     this.value = value
     return this.result
--- backend/src/data/usecases/account/add-account/db-add-account.spec.ts
+++ backend/src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -1,3 +1,4 @@
+import { AddAccountRepositorySpy } from './../../../test/mock-db-account';
 import { throwError } from './../../../../domain/test/test-helper';
 import { mockAddAccountParams } from './../../../../domain/test/mock-account';
 import { DbAddAccount } from './db-add-account';
@@ -6,14 +7,17 @@ import { HasherSpy } from './../../../test/mock-criptography';
 type SutTypes = {
   sut: DbAddAccount
   hasherSpy: HasherSpy
+  addAccountRepositorySpy: AddAccountRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const hasherSpy = new HasherSpy()
-  const sut = new DbAddAccount(hasherSpy)
+  const addAccountRepositorySpy = new AddAccountRepositorySpy()
+  const sut = new DbAddAccount(hasherSpy, addAccountRepositorySpy)
   return {
     sut,
-    hasherSpy
+    hasherSpy,
+    addAccountRepositorySpy
   }
 }
 
@@ -31,5 +35,17 @@ describe('DbAddAccount', () => {
     const accountData = mockAddAccountParams()
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  });
+
+  test('should call AddAccountRepository with correct values', async () => {
+    const { sut, addAccountRepositorySpy } = makeSut()
+    const accountData = mockAddAccountParams()
+    await sut.add(accountData)
+    expect(addAccountRepositorySpy.data).toEqual({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      cpf: 'any_cpf',
+      password: 'hashed_password'
+    })
   });
 });
