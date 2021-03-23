3e2614d43e23266ea620a5c5e5978b96216b2fd6
Wisinewski <g_gomees@hotmail.com>
2021-03-23 19:07:54 -0300

    test: create mock account model method

--- backend/src/domain/test/mock-account.ts
+++ backend/src/domain/test/mock-account.ts
@@ -1,3 +1,4 @@
+import { AccountModel } from './../model/account';
 import { AddAccountParams } from './../usecases/account/add-account';
 
 export const mockAddAccountParams = (): AddAccountParams => ({
@@ -5,4 +6,12 @@ export const mockAddAccountParams = (): AddAccountParams => ({
   email: 'any_email@email.com',
   cpf: 'any_cpf',
   password: 'any_password'
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@email.com',
+  cpf: 'any_cpf',
+  password: 'hashed_password'
 })
