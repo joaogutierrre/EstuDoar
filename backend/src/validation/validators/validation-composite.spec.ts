10d9b4823cd9edb28c5ef5578332fdbb077b348e
Wisinewski <g_gomees@hotmail.com>
2021-03-24 20:10:17 -0300

    test: ensure ValidationComposite not returns an error if validation succeeds

--- backend/src/validation/validators/validation-composite.spec.ts
+++ backend/src/validation/validators/validation-composite.spec.ts
@@ -24,4 +24,18 @@ describe('ValidationComposite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   });
+
+  test('should return the first error if more than one validation fails', () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new Error())
+  });
+
+  test('should not return an error if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  });
 });
