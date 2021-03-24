01795fa7b7365b16ffacae687b559cf05fe8e46c
Wisinewski <g_gomees@hotmail.com>
2021-03-23 23:28:39 -0300

    feat: create mongodb helper class

---
+++ backend/src/infra/helpers/mongo-helper.ts
@@ -0,0 +1,35 @@
+import { Collection, MongoClient } from "mongodb"
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  uri: null as string,
+
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+    this.client = null
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client || !this.client.isConnected()) {
+      await this.connect(this.url)
+    }
+    return this.client.db().collection(name)
+  },
+
+  map: (data: any): any => {
+    const { _id, ...collectionWithoutId } = data
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  },
+
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map(c => MongoHelper.map(c))
+  }
+}
