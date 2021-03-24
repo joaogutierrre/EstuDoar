55a8e7e6eb5bdd528eb4cdad9a695339f20af6fe
Wisinewski <g_gomees@hotmail.com>
2021-03-23 22:25:00 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

--- backend/src/infra/validators/email-validator-adapter.spec.ts
+++ backend/src/infra/validators/email-validator-adapter.spec.ts
@@ -25,4 +25,11 @@ describe('Name of the group', () => {
     sut.isValid('any_email@email.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@email.com')
   });
+
+  test('should returns false if validator returns false', () => {
+    const { sut } = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('any_email@email.com')
+    expect(isValid).toBe(false)
+  });
 });
