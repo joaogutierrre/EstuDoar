04786f9722cd749015fa4a3425a57cdab6959249
Wisinewski <g_gomees@hotmail.com>
2021-03-24 18:14:30 -0300

    feat: create ok response helper method

--- backend/src/presentation/helpers/http/http-helper.ts
+++ backend/src/presentation/helpers/http/http-helper.ts
@@ -1,12 +1,17 @@
 import { ServerError } from './../../errors/server-error';
 import { HttpResponse } from './../../protocols/http';
 
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500,
-  body: new ServerError(error.stack)
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
 })
 
 export const forbidden = (error: Error): HttpResponse => ({
   statusCode: 403,
   body: error 
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
