d791aae161582c8bd057de7bed3b0d7c524ec65c
Wisinewski <g_gomees@hotmail.com>
2021-03-24 18:24:32 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- backend/src/presentation/controllers/signup-controller.ts
+++ backend/src/presentation/controllers/signup-controller.ts
@@ -1,12 +1,14 @@
+import { Authentication } from './../../domain/usecases/account/authentication';
 import { EmailInUseError } from './../errors/email-in-use-error';
-import { serverError, forbidden } from './../helpers/http/http-helper';
+import { serverError, forbidden, ok } from './../helpers/http/http-helper';
 import { AddAccount } from './../../domain/usecases/account/add-account';
 import { HttpRequest, HttpResponse } from './../protocols/http';
 import { Controller } from './../protocols/controller';
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly addAccount: AddAccount
+    private readonly addAccount: AddAccount,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -21,6 +23,11 @@ export class SignUpController implements Controller {
       if (!account) {
         return forbidden(new EmailInUseError())
       }
+      const accessToken = await this.authentication.auth({
+        email,
+        password
+      })
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
