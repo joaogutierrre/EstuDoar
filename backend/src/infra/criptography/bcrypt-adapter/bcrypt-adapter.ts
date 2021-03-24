d8d68c18447abb53c45aa4498743b16bff2fa932
Wisinewski <g_gomees@hotmail.com>
2021-03-23 22:07:13 -0300

    feat: ensure BcryptAdapter returns a valid hash on hash success

--- backend/src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ backend/src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -7,7 +7,7 @@ export class BcryptAdapter implements Hasher {
   ) {}
 
   async hash (value: string): Promise<string> {
-    bcrypt.hash(value, this.salt)
-    return null
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
   }
 }
