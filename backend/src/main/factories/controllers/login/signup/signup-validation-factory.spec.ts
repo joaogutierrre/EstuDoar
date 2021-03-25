6cbbc5cc6ae175739f391d628e45f203edbd4bf2
Wisinewski <g_gomees@hotmail.com>
2021-03-25 02:03:08 -0300

    test: ensure SignUpValidationFactory calls ValidationComposite with all validations

---
+++ backend/src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,31 @@
+import { EmailValidator } from './../../../../../validation/protocols/email-validator';
+import { Validation } from './../../../../../presentation/protocols/validation';
+import { EmailValidation } from './../../../../../validation/validators/email-validation';
+import { CompareFieldsValidation } from './../../../../../validation/validators/compare-fields-validation';
+import { ValidationComposite } from './../../../../../validation/validators/validation-composite';
+import { RequiredFieldValidation } from './../../../../../validation/validators/required-field-validation';
+import { makeSignUpValidation } from './signup-validation-factory';
+
+jest.mock('../../../../../validation/validators/validation-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+describe('SignUpValidation factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeSignUpValidation()
+    const validations: Validation[] = []
+    for (const field of ['name', 'email', 'cpf', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  });
+});
