a971cc260f19ffb11a33f27bc0536ed4ade4de94
Wisinewski <g_gomees@hotmail.com>
2021-03-26 18:43:19 -0300

    test: ensure ApiLoadCitiesByUf throws if LoadCitiesByUfService throws

--- backend/src/data/usecases/locality/load-cities-by-uf/api-load-cities-by-uf.spec.ts
+++ backend/src/data/usecases/locality/load-cities-by-uf/api-load-cities-by-uf.spec.ts
@@ -1,3 +1,5 @@
+import { throwError } from './../../../../domain/test/test-helper';
+import { LoadCitiesByUfService } from './../../../protocols/service/load-cities-by-uf-service';
 import { ApiLoadCitiesByUf } from './api-load-cities-by-uf';
 import { LoadCitiesByUfServiceSpy } from './../../../test/mock-locality';
 
@@ -18,8 +20,15 @@ const makeSut = (): SutTypes => {
 describe('ApiLoadCitiesByUf', () => {
   test('should call LoadCitiesByUfService with correct value', async () => {
     const { sut, loadCitiesByUfServiceSpy } = makeSut()
-    const ufValue = 'uf'
+    const ufValue = 'any_uf'
     await sut.load(ufValue)
     expect(loadCitiesByUfServiceSpy.data).toBe(ufValue)
+  });
+
+  test('should throw if LoadCitiesByUfService throws', () => {
+    const { sut, loadCitiesByUfServiceSpy } = makeSut()
+    jest.spyOn(loadCitiesByUfServiceSpy, 'loadCitiesByUf').mockImplementationOnce(throwError)
+    const promise = sut.load('any_uf')
+    expect(promise).rejects.toThrow()
   });
 });
