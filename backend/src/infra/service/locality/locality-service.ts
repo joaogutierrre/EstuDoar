4fd465cd646da6cf8ae98b25999c6e497f354080
Wisinewski <g_gomees@hotmail.com>
2021-03-26 20:48:32 -0300

    feat: ensure LocalityService throws if axios throws

--- backend/src/infra/service/locality/locality-service.ts
+++ backend/src/infra/service/locality/locality-service.ts
@@ -1,7 +1,8 @@
+import { LoadCitiesByUfService } from './../../../data/protocols/service/load-cities-by-uf-service';
 import { LoadUfsService } from './../../../data/protocols/service/load-ufs-service';
 import axios from 'axios'
 
-export class LocalityService implements LoadUfsService {
+export class LocalityService implements LoadUfsService, LoadCitiesByUfService {
   async loadAllUfs (): Promise<any> {
     try {
       const response = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
@@ -9,5 +10,13 @@ export class LocalityService implements LoadUfsService {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  async loadCitiesByUf (uf: string): Promise<any> {
+    const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+    if (response.data.statusCode !== 200) {
+      return null
+    }
+    return response.data
   }
 }
