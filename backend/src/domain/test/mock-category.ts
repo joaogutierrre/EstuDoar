ea546c4ec297e9b06554de5ba2da79a7e1f8aa6d
João Gutierre <gutierrejoao@gmail.com>
2021-03-27 20:25:57 -0300

    refactor: remove unnecessary comment

--- backend/src/domain/test/mock-category.ts
+++ backend/src/domain/test/mock-category.ts
@@ -1,8 +1,5 @@
 import { CategoriesModel } from "../model/category";
 
-// export const mockLoadCategory = (): LoadCategory => ({  
-// })
-
 export const mockCategoryModel = (): CategoriesModel => ({
     categories: [{
         id: 'any_id',
