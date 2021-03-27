45922c0530abcc6c3d00d11029d0b397d400fecc
João Gutierre <gutierrejoao@gmail.com>
2021-03-27 19:16:31 -0300

    feat: ensure DbLoadCategory returns null if LoadCategoryRepository returns null

--- backend/src/data/usecases/category/db-load-category.ts
+++ backend/src/data/usecases/category/db-load-category.ts
@@ -1,15 +1,18 @@
-import { CategoryModel } from '../../../domain/model/category';
+import { CategoriesModel } from '../../../domain/model/category';
 import { LoadCategories } from '../../../domain/usecases/category/load-category';
-import { LoadCategoryRepository} from './../../protocols/db/load-category-repository'
+import { LoadCategoryRepository } from './../../protocols/db/load-category-repository';
 
 export class DbLoadCategory implements LoadCategories {
 
     constructor(
         private readonly loadCategoryRepository: LoadCategoryRepository
-    ){}
+    ) { }
 
-    async load(): Promise<CategoryModel[]> {
-        await this.loadCategoryRepository.loadCategory()
+    async load(): Promise<CategoriesModel> {
+        const categories = await this.loadCategoryRepository.loadCategory()
+        if (!categories) {
+            return categories
+        }
         return null
     }
 
