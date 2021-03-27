1390588e474b30f69d406b17f37665b1a2f2174c
João Gutierre <gutierrejoao@gmail.com>
2021-03-27 19:28:59 -0300

    Revert "feat: ensure DbLoadCategory returns null if LoadCategoryRepository returns null"
    This reverts commit 45922c0530abcc6c3d00d11029d0b397d400fecc.

--- backend/src/data/usecases/category/db-load-category.ts
+++ backend/src/data/usecases/category/db-load-category.ts
@@ -1,18 +1,15 @@
-import { CategoriesModel } from '../../../domain/model/category';
+import { CategoryModel } from '../../../domain/model/category';
 import { LoadCategories } from '../../../domain/usecases/category/load-category';
-import { LoadCategoryRepository } from './../../protocols/db/load-category-repository';
+import { LoadCategoryRepository} from './../../protocols/db/load-category-repository'
 
 export class DbLoadCategory implements LoadCategories {
 
     constructor(
         private readonly loadCategoryRepository: LoadCategoryRepository
-    ) { }
+    ){}
 
-    async load(): Promise<CategoriesModel> {
-        const categories = await this.loadCategoryRepository.loadCategory()
-        if (!categories) {
-            return categories
-        }
+    async load(): Promise<CategoryModel[]> {
+        await this.loadCategoryRepository.loadCategory()
         return null
     }
 
