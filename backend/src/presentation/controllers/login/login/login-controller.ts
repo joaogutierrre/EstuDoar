a38899d6e4bca7da163288c483c0366ca5ca5478
Wisinewski <g_gomees@hotmail.com>
2021-03-27 19:29:08 -0300

    feat: ensure LoginController calls Validation with correct values

--- backend/src/presentation/controllers/login/login/login-controller.ts
+++ backend/src/presentation/controllers/login/login/login-controller.ts
@@ -1,16 +1,22 @@
+import { Validation } from './../../../protocols/validation';
 import { ServerError } from './../../../errors/server-error';
-import { serverError, unauthorized, ok } from './../../../helpers/http/http-helper';
+import { serverError, unauthorized, ok, badRequest } from './../../../helpers/http/http-helper';
 import { Authentication } from './../../../../domain/usecases/account/authentication';
 import { HttpRequest, HttpResponse } from './../../../protocols/http';
 import { Controller } from './../../../protocols/controller';
 
 export class LoginController implements Controller {
   constructor (
-    private readonly authentication: Authentication
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { email, password } = httpRequest.body
       const accessToken = await this.authentication.auth({ email, password })
       if (!accessToken) {
