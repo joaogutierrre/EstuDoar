5a28586b8d5062330a5821fe4d0ca3102924a993
Wisinewski <g_gomees@hotmail.com>
2021-03-27 16:55:25 -0300

    feat: ensure DbAuthentication calls Encrypter with correct value

--- backend/src/data/usecases/account/authentication/db-authentication.ts
+++ backend/src/data/usecases/account/authentication/db-authentication.ts
@@ -1,3 +1,4 @@
+import { Encrypter } from './../../../protocols/criptography/encrypter';
 import { HashComparer } from './../../../protocols/criptography/hash-comparer';
 import { LoadAccountByEmailRepository } from './../../../protocols/db/load-account-by-email-repository';
 import { Authentication, AuthenticationParams } from './../../../../domain/usecases/account/authentication';
@@ -5,14 +6,18 @@ import { Authentication, AuthenticationParams } from './../../../../domain/useca
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth (data: AuthenticationParams): Promise<string> {
     const { email, password } = data
     const account = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (account) {
-      await this.hashComparer.compare(password, account.password)
+      const isValid = await this.hashComparer.compare(password, account.password)
+      if (isValid) {
+        this.encrypter.encrypt(account.id)
+      }
     }
     return null
   }
