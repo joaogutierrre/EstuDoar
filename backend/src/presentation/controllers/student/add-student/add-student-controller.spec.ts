121739e41914b537ac367b24e027b1b9aee6278b
Wisinewski <g_gomees@hotmail.com>
2021-03-28 17:53:43 -0300

    test: ensure AddStudentController returns 500 if AddStudent throws

---
+++ backend/src/presentation/controllers/student/add-student/add-student-controller.spec.ts
@@ -0,0 +1,42 @@
+import { HttpRequest } from './../../../protocols/http';
+import { serverError } from './../../../helpers/http/http-helper';
+import { AddStudentController } from './add-student-controller';
+import { AddStudentSpy } from './../../../test/mock-student';
+
+const mockRequest = (): HttpRequest => ({
+  body: {
+    accountId: 'any_id',
+    name: 'any_name',
+    school: 'any_school',
+    items: [{
+      category: 'any_category',
+      quantity: 'any_quantity'
+    }]
+  }
+})
+
+type SutTypes = {
+  sut: AddStudentController
+  addStudentSpy: AddStudentSpy
+}
+
+const makeSut = (): SutTypes => {
+  const addStudentSpy = new AddStudentSpy()
+  const sut = new AddStudentController(addStudentSpy)
+  return {
+    sut,
+    addStudentSpy
+  }
+}
+
+describe('AddStudentController', () => {
+  test('should return 500 if AddStudent throws', async () => {
+    const { sut, addStudentSpy } = makeSut()
+    jest.spyOn(addStudentSpy, 'add').mockImplementationOnce(async () => {
+      return Promise.reject(new Error())
+    })
+    const httpRequest = mockRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(serverError(new Error()))
+  });
+});
