9e7ea9b512d3455f11eade86c7df4fb5a95b4cfd
Wisinewski <g_gomees@hotmail.com>
2021-03-28 15:13:54 -0300

    test: ensure DbAddAccount throws if AddStudentRepository throws

--- backend/src/data/usecases/student/add-student/db-add-student.spec.ts
+++ backend/src/data/usecases/student/add-student/db-add-student.spec.ts
@@ -1,3 +1,4 @@
+import { throwError } from './../../../../domain/test/test-helper';
 import { AddStudentRepositorySpy } from './../../../test/mock-db-student';
 import { mockAddStudentParams } from './../../../../domain/test/mock-student';
 import { DbAddStudent } from './db-add-student';
@@ -22,5 +23,12 @@ describe('DbAddStudent', () => {
     const { sut, addStudentRepositorySpy } = makeSut()
     await sut.add(mockAddStudentParams())
     expect(addStudentRepositorySpy.data).toEqual(mockAddStudentParams())
+  });
+
+  test('should throw if AddStudentRepository throws', async () => {
+    const { sut, addStudentRepositorySpy } = makeSut()
+    jest.spyOn(addStudentRepositorySpy, 'add').mockImplementationOnce(throwError)
+    const promise = sut.add(mockAddStudentParams())
+    await expect(promise).rejects.toThrow()
   });
 });
