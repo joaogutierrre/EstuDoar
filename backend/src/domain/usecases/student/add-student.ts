163283b1128deaca64ef66d0c5717f59398ccb6a
Wisinewski <g_gomees@hotmail.com>
2021-03-28 16:38:10 -0300

    refactor: change add student usecase return type

--- backend/src/domain/usecases/student/add-student.ts
+++ backend/src/domain/usecases/student/add-student.ts
@@ -3,5 +3,5 @@ import { StudentModel } from './../../model/student';
 export type AddStudentParams = Omit<StudentModel, 'id'>
 
 export interface AddStudent {
-  add: (data: AddStudentParams) => Promise<void>
+  add: (data: AddStudentParams) => Promise<StudentModel>
 }
