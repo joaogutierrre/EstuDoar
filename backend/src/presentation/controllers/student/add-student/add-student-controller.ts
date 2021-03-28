8e8930233ec2b911ce9debb0c37a65e03699b17e
Wisinewski <g_gomees@hotmail.com>
2021-03-28 18:03:59 -0300

    feat: ensure AddStudentController calls Validation with correct values

--- backend/src/presentation/controllers/student/add-student/add-student-controller.ts
+++ backend/src/presentation/controllers/student/add-student/add-student-controller.ts
@@ -1,15 +1,21 @@
-import { serverError } from './../../../helpers/http/http-helper';
+import { Validation } from './../../../protocols/validation';
+import { serverError, badRequest } from './../../../helpers/http/http-helper';
 import { AddStudent } from './../../../../domain/usecases/student/add-student';
 import { HttpRequest, HttpResponse } from './../../../protocols/http';
 import { Controller } from './../../../protocols/controller';
 
 export class AddStudentController implements Controller {
   constructor (
-    private readonly addStudent: AddStudent
+    private readonly addStudent: AddStudent,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const { accountId, name, school, items } = httpRequest.body
       await this.addStudent.add({
         accountId,
