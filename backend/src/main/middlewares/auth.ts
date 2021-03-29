ee25e1918fad4f124aa5519d9e2791f0add6d15a
Wisinewski <g_gomees@hotmail.com>
2021-03-29 02:39:51 -0300

    feat: create adapt middlewares helper methods

--- backend/src/main/middlewares/auth.ts
+++ backend/src/main/middlewares/auth.ts
@@ -2,3 +2,9 @@ import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-fac
 import { adaptMiddleware } from './../adapters/express-middleware-adapter';
 
 export const auth = adaptMiddleware(makeAuthMiddleware())
+
+export const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+
+export const donatorAuth = adaptMiddleware(makeAuthMiddleware('donator'))
+
+export const parentAuth = adaptMiddleware(makeAuthMiddleware('parent'))
