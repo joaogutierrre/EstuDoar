214d303eeadbe8c8e4537afbd71ba3ae8329715c
Wisinewski <g_gomees@hotmail.com>
2021-03-29 01:01:49 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- backend/src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ backend/src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -54,5 +54,11 @@ describe('JwtAdapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     });
+
+    test('should return a decrypted value on verify success', async () => {
+      const { sut } = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    });
   });
 });
