2cd6ea46e23ef651396e18c2ba859cd7acd0fe85
Wisinewski <g_gomees@hotmail.com>
2021-03-29 19:50:45 -0300

    feat: ensure LoadStudentsByAccount returns 204 if LoadStudentsByAccount returns an empty list

--- backend/src/presentation/controllers/student/load-students-by-account/load-students-by-account-controller.ts
+++ backend/src/presentation/controllers/student/load-students-by-account/load-students-by-account-controller.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from './../../../errors/invalid-param-error';
-import { serverError, forbidden, ok } from './../../../helpers/http/http-helper';
+import { serverError, forbidden, ok, noContent } from './../../../helpers/http/http-helper';
 import { LoadStudentsByAccount } from './../../../../domain/usecases/student/load-students-by-account';
 import { HttpRequest, HttpResponse } from './../../../protocols/http';
 import { Controller } from './../../../protocols/controller';
@@ -16,7 +16,7 @@ export class LoadStudentsByAccountController implements Controller {
       if (!students) {
         return forbidden(new InvalidParamError('accountId'))
       }
-      return ok(students)
+      return students.length ? ok(students) : noContent()
     } catch (error) {
       return serverError(error)
     } 
