bfe1a4de4c6814438532fc4f2215d975382e0313
Wisinewski <g_gomees@hotmail.com>
2021-03-29 18:49:18 -0300

    feat: ensure StudentMongoRepository returns all students from an account

--- backend/src/infra/db/mongodb/student/student-mongo-repository.ts
+++ backend/src/infra/db/mongodb/student/student-mongo-repository.ts
@@ -1,13 +1,23 @@
+import { LoadStudentsByAccountRepository } from './../../../../data/protocols/db/student/load-students-by-account-repository';
 import { MongoHelper } from './../helpers/mongo-helper';
 import { AddStudentParams } from './../../../../domain/usecases/student/add-student';
 import { StudentModel } from './../../../../domain/model/student';
 import { AddStudentRepository } from '../../../../data/protocols/db/student/add-student-repository';
 
-export class StudentMongoRepository implements AddStudentRepository {
+export class StudentMongoRepository implements AddStudentRepository, LoadStudentsByAccountRepository {
   async add (data: AddStudentParams): Promise<StudentModel> {
     const studentCollection = await MongoHelper.getCollection('students')
     const result = await studentCollection.insertOne(data)
     const student = result.ops[0]
     return MongoHelper.map(student)
+  }
+
+  async loadStudentsByAccount (accountId: string): Promise<StudentModel[]> {
+    const studentCollection = await MongoHelper.getCollection('students')
+    const students = await studentCollection.find({
+      accountId: accountId
+    })
+    .toArray()
+    return students && MongoHelper.mapCollection(students)
   }
 }
