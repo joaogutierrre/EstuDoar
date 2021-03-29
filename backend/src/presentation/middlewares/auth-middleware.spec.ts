2ce27bd04701f7e4ea22ca56aba126e0f69cd389
Wisinewski <g_gomees@hotmail.com>
2021-03-29 00:02:57 -0300

    test: ensure AuthMiddleware returns 403 if no x-access-token exists in headers

---
+++ backend/src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,33 @@
+import { LoadAccountByTokenSpy } from './../test/mock-account';
+import { AccessDeniedError } from './../errors/access-email-error';
+import { forbidden } from './../helpers/http/http-helper';
+import { HttpRequest } from './../protocols/http';
+import { AuthMiddleware } from './auth-middleware';
+
+const mockRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': 'any_token'
+  }
+})
+
+type SutTypes = {
+  sut: AuthMiddleware
+  loadAccountByTokenSpy: LoadAccountByTokenSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenSpy = new LoadAccountByTokenSpy()
+  const sut = new AuthMiddleware(loadAccountByTokenSpy)
+  return {
+    sut,
+    loadAccountByTokenSpy
+  }
+}
+
+describe('AuthMiddleware', () => {
+  test('should return 403 if no x-access-token exists in headers', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  });
+});
--- backend/src/presentation/test/mock-account.ts
+++ backend/src/presentation/test/mock-account.ts
@@ -1,6 +1,7 @@
+import { AccountModel } from './../../domain/model/account';
+import { LoadAccountByToken } from './../../domain/usecases/account/load-account-by-token';
 import { Authentication, AuthenticationParams } from '../../domain/usecases/account/authentication';
 import { mockAccountModel } from '../../domain/test/mock-account';
-import { AccountModel } from '../../domain/model/account';
 import { AddAccount, AddAccountParams } from '../../domain/usecases/account/add-account';
 
 export class AddAccountSpy implements AddAccount {
@@ -22,5 +23,15 @@ export class AuthenticationSpy implements Authentication {
     const accessToken = 'any_token'
     this.result = accessToken
     return accessToken
+  }
+}
+
+export class LoadAccountByTokenSpy implements LoadAccountByToken {
+  accessToken: string
+  role?: string
+  async load (accessToken: string, role?: string): Promise<AccountModel> {
+    this.accessToken = accessToken
+    this.role = role
+    return mockAccountModel()
   }
 }
