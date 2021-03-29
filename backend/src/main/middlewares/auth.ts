32b78ca0918eac279a89623ef85a14a3b975d886
Wisinewski <g_gomees@hotmail.com>
2021-03-29 02:33:29 -0300

    feat: create auth middleware

---
+++ backend/src/main/middlewares/auth.ts
@@ -0,0 +1,4 @@
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory';
+import { adaptMiddleware } from './../adapters/express-middleware-adapter';
+
+export const auth = adaptMiddleware(makeAuthMiddleware())
