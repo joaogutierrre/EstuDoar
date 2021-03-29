94e56d2402072020705626c5882976df5f32360a
Wisinewski <g_gomees@hotmail.com>
2021-03-29 01:44:35 -0300

    test: ensure AccountMongoRepository returns null on loadByToken with invalid role

--- backend/src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ backend/src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -111,5 +111,34 @@ describe('AccountMongoRepository', () => {
       expect(account.email).toBe('any_email@email.com')
       expect(account.password).toBe('any_password')
     });
+
+    test('should return an account on loadByToken if user is admin', async () => {
+      const { sut } = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'admin'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@email.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    test('should return null on loadByToken with invalid role', async () => {
+      const { sut } = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token', 'admin')
+      expect(account).toBeFalsy()
+    })
   });
 });
