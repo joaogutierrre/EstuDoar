dd23884867aa325d713281a10654c0d1ec6ba156
Wisinewski <g_gomees@hotmail.com>
2021-03-29 15:29:13 -0300

    test: add role parameter to makeAccessToken helper method

--- backend/src/main/routes/student-routes.test.ts
+++ backend/src/main/routes/student-routes.test.ts
@@ -6,18 +6,19 @@ import env from '../config/env';
 import { sign } from 'jsonwebtoken';
 
 let studentCollection: Collection
+let accountCollection: Collection
 
-const makeAccessToken = async (): Promise<string> => {
-  const res = await studentCollection.insertOne({
+const makeAccessToken = async (role: string): Promise<string> => {
+  const res = await accountCollection.insertOne({
     name: 'any_name',
     email: 'any_email@email.com',
     cpf: '11111111111',
     password: '123',
-    role: 'admin'
+    role: role
   })
   const id = res.ops[0]._id
   const accessToken = sign({ id }, env.jwtSecret)
-  await studentCollection.updateOne({
+  await accountCollection.updateOne({
     _id: id
   }, {
     $set: {
@@ -39,6 +40,8 @@ describe('Student Routes', () => {
   beforeEach(async () => {
     studentCollection = await MongoHelper.getCollection('students')
     await studentCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /students', () => {
@@ -60,7 +63,7 @@ describe('Student Routes', () => {
     });
 
     test('should return 200 on add student with valid accessToken', async () => {
-      const accessToken = await makeAccessToken()
+      const accessToken = await makeAccessToken('parent')
       await request(app)
         .post('/api/students')
         .set('x-access-token', accessToken)
@@ -76,6 +79,25 @@ describe('Student Routes', () => {
           }]
         })
         .expect(200)
+    })
+
+    test('should return 403 on add student with invalid role', async () => {
+      const accessToken = await makeAccessToken('donator')
+      await request(app)
+        .post('/api/students')
+        .set('x-access-token', accessToken)
+        .send({
+          name: 'any_name',
+          school: 'any_school',
+          items: [{
+            category: 'any_category',
+            quantity: 1
+          }, {
+            category: 'other_category',
+            quantity: 2
+          }]
+        })
+        .expect(403)
     })
   });
 });
