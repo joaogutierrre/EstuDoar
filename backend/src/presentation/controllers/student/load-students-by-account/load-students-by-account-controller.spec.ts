8aaee69043c6e1951edfd1c4ea93b2c3bc05232c
Wisinewski <g_gomees@hotmail.com>
2021-03-29 19:34:55 -0300

    test: ensure LoadStudentsByAccountController returns 500 if LoadStudentsByAccount throws

---
+++ backend/src/presentation/controllers/student/load-students-by-account/load-students-by-account-controller.spec.ts
@@ -0,0 +1,34 @@
+import { serverError } from './../../../helpers/http/http-helper';
+import { LoadStudentsByAccountSpy } from './../../../test/mock-student';
+import { LoadStudentsByAccountController } from './load-students-by-account-controller';
+import { HttpRequest } from './../../../protocols/http';
+import { throwError } from './../../../../domain/test/test-helper';
+
+const mockRequest = (): HttpRequest => ({
+  body: {
+    accountId: 'any_id'
+  }
+})
+
+type SutTypes = {
+  sut: LoadStudentsByAccountController
+  loadStudentsByAccountSpy: LoadStudentsByAccountSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadStudentsByAccountSpy = new LoadStudentsByAccountSpy()
+  const sut = new LoadStudentsByAccountController(loadStudentsByAccountSpy)
+  return {
+    sut, 
+    loadStudentsByAccountSpy
+  }
+}
+
+describe('LoadStudentsByAccount Controller', () => {
+  test('should return 500 if LoadStudentsByAccount throws', async () => {
+    const { sut, loadStudentsByAccountSpy } = makeSut()
+    jest.spyOn(loadStudentsByAccountSpy, 'load').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  });
+});
--- backend/src/presentation/test/mock-student.ts
+++ backend/src/presentation/test/mock-student.ts
@@ -1,4 +1,5 @@
-import { mockStudentModel } from './../../domain/test/mock-student';
+import { LoadStudentsByAccount } from './../../domain/usecases/student/load-students-by-account';
+import { mockStudentModel, mockStudentModelList } from './../../domain/test/mock-student';
 import { StudentModel } from './../../domain/model/student';
 import { AddStudent, AddStudentParams } from './../../domain/usecases/student/add-student';
 
@@ -9,5 +10,15 @@ export class AddStudentSpy implements AddStudent {
     this.data = data
     this.result = mockStudentModel()
     return mockStudentModel()
+  }
+}
+
+export class LoadStudentsByAccountSpy implements LoadStudentsByAccount {
+  data: string
+  result: object
+  async load (accountId: string): Promise<StudentModel[]> {
+    this.data = accountId
+    this.result = mockStudentModelList()
+    return mockStudentModelList()
   }
 }
