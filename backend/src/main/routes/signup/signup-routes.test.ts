74318074a1a4b099cc41ca0f9bef257320594166
João Gutierre <gutierrejoao@gmail.com>
2021-03-30 15:43:03 -0300

    refactor: move files to their right directory

--- backend/src/main/routes/signup/signup-routes.test.ts
+++ backend/src/main/routes/signup/signup-routes.test.ts
@@ -1,6 +1,6 @@
-import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+import { MongoHelper } from '../../../infra/db/mongodb/helpers/mongo-helper';
 import request from 'supertest'
-import app from '../config/app'
+import app from '../../config/app'
 
 describe('SignUp Routes', () => {
   beforeAll(async () => {
