e0e3096a84610bc3ded9e3c2aa707c5944126305
João Gutierre <gutierrejoao@gmail.com>
2021-03-29 22:50:40 -0300

    refactor: move presentation/controller/signup && presentation/test/account files

--- backend/src/presentation/controllers/signup/signup-controller.ts
+++ backend/src/presentation/controllers/signup/signup-controller.ts
@@ -1,10 +1,10 @@
-import { Validation } from '../protocols/validation';
-import { Authentication } from '../../domain/usecases/account/authentication';
-import { EmailInUseError } from '../errors/email-in-use-error';
-import { serverError, forbidden, ok, badRequest } from '../helpers/http/http-helper';
-import { AddAccount } from '../../domain/usecases/account/add-account';
-import { HttpRequest, HttpResponse } from '../protocols/http';
-import { Controller } from '../protocols/controller';
+import { Validation } from '../../protocols/validation';
+import { Authentication } from '../../../domain/usecases/account/authentication';
+import { EmailInUseError } from '../../errors/email-in-use-error';
+import { serverError, forbidden, ok, badRequest } from '../../helpers/http/http-helper';
+import { AddAccount } from '../../../domain/usecases/account/add-account';
+import { HttpRequest, HttpResponse } from '../../protocols/http';
+import { Controller } from '../../protocols/controller';
 
 export class SignUpController implements Controller {
   constructor (
