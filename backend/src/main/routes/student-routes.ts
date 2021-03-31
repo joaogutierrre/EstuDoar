68121a56a6c7f45d554d3a80db01688a6e8c5ccf
Wisinewski <g_gomees@hotmail.com>
2021-03-31 19:39:49 -0300

    feat: create update student by id route

--- backend/src/main/routes/student-routes.ts
+++ backend/src/main/routes/student-routes.ts
@@ -1,3 +1,4 @@
+import { makeUpdateStudentByIdController } from './../factories/controllers/student/update-student-by-id/update-student-by-id-controller-factory';
 import { makeLoadStudentsByAccountController } from './../factories/controllers/student/load-students-by-account/load-students-by-account-factory';
 import { makeAddStudentController } from './../factories/controllers/student/add-student/add-student-controller-factory';
 import { adaptRoute } from './../adapters/express-route-adapter';
@@ -7,4 +8,5 @@ import { Router } from 'express';
 export default (router: Router): void => {
   router.post('/students', parentAuth, adaptRoute(makeAddStudentController()))
   router.get('/students', parentAuth, adaptRoute(makeLoadStudentsByAccountController()))
+  router.put('/students', parentAuth, adaptRoute(makeUpdateStudentByIdController()))
 }
