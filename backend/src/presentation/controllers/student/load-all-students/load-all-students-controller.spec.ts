17509e647e46ddcf8c05323b3c6ea8ad88d9bdc8
Wisinewski <g_gomees@hotmail.com>
2021-04-01 17:33:25 -0300

    test: ensure LoadAllStudentsController returns 500 if LoadAllStudents throws

---
+++ backend/src/presentation/controllers/student/load-all-students/load-all-students-controller.spec.ts
@@ -0,0 +1,37 @@
+import { serverError } from './../../../helpers/http/http-helper';
+import { throwError } from './../../../../domain/test/test-helper';
+import { LoadAllStudentsSpy } from './../../../test/mock-student';
+import { LoadAllStudentsController } from './load-all-students-controller';
+import { HttpRequest } from './../../../protocols/http';
+
+const mockRequest = (): HttpRequest => ({
+  body: {
+    uf: 'any_uf',
+    city: 'any_city',
+    subDistrict: 'any_subDistrict',
+    school: 'any_school'
+  }
+})
+
+type SutTypes = {
+  sut: LoadAllStudentsController
+  loadAllStudentsSpy: LoadAllStudentsSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadAllStudentsSpy = new LoadAllStudentsSpy()
+  const sut = new LoadAllStudentsController(loadAllStudentsSpy)
+  return {
+    sut, 
+    loadAllStudentsSpy
+  }
+}
+
+describe('LoadAllStudents Controller', () => {
+  test('should return 500 if LoadAllStudents throws', async () => {
+    const { sut, loadAllStudentsSpy } = makeSut()
+    jest.spyOn(loadAllStudentsSpy, 'load').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  });
+});
