1c2af94919884be628447533889c87cde6503108
João Gutierre <gutierrejoao@gmail.com>
2021-04-01 17:08:14 -0300

    feat: create mock-load-subdisctrict file

---
+++ backend/src/data/test/mock-load-subdistrict.ts
@@ -0,0 +1,20 @@
+import { SubdistrictModel } from "../../domain/model/subdistrict";
+import { LoadSubdistrictRepository } from "../protocols/db/subdistrict/load-subdistrict-repository";
+
+export class LoadSubdistrictRepositorySpy implements LoadSubdistrictRepository{
+    data: string
+    result: object
+    async loadSubdistrict(): Promise<SubdistrictModel[]>{
+        const fakeSubdistrict = [{
+            id: 'any_id',
+            code: 'any_code',
+            name: 'any_name'
+        },{
+            id: 'other_id',
+            code: 'other_code',
+            name: 'other_name'
+        }]
+        this.result = fakeSubdistrict
+        return fakeSubdistrict
+    }
+}
