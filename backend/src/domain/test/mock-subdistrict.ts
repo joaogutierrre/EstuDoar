aa57b385892130a9cfc9c267dd6f9305cb26993d
João Gutierre <gutierrejoao@gmail.com>
2021-04-01 16:40:11 -0300

    feat: create mock-load-subdistrict file

---
+++ backend/src/domain/test/mock-subdistrict.ts
@@ -0,0 +1,13 @@
+import { SubdistrictsModel } from "../model/subdistrict";
+
+export const mockSubdistrictModel = (): SubdistrictsModel => ({
+    subdistricts: [{
+        id: 'any_id',
+        code: 'any_code',
+        name: 'any_name'
+    }, {
+        id: 'other_id',
+        code: 'other_code',
+        name: 'other_name'
+    }]
+})
