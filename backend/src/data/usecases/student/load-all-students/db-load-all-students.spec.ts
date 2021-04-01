13f60458c641b4b2c77e4a4fb2402c3ed30321e9
Wisinewski <g_gomees@hotmail.com>
2021-04-01 15:41:28 -0300

    test: ensure DbLoadAllStudents returns a list of students on success

--- backend/src/data/usecases/student/load-all-students/db-load-all-students.spec.ts
+++ backend/src/data/usecases/student/load-all-students/db-load-all-students.spec.ts
@@ -1,7 +1,7 @@
 import { throwError } from './../../../../domain/test/test-helper';
 import { LoadAllStudentsRepositorySpy } from './../../../test/mock-db-student';
 import { DbLoadAllStudents } from './db-load-all-students';
-import { mockLoadAllStudentsParams } from './../../../../domain/test/mock-student';
+import { mockLoadAllStudentsParams, mockStudentModelList } from './../../../../domain/test/mock-student';
 type SutTypes = {
   sut: DbLoadAllStudents
   loadAllStudentsRepositorySpy: LoadAllStudentsRepositorySpy
@@ -29,5 +29,11 @@ describe('DbLoadAllStudents', () => {
     jest.spyOn(loadAllStudentsRepositorySpy, 'loadAllStudents').mockImplementationOnce(throwError)
     const promise = sut.load(mockLoadAllStudentsParams())
     await expect(promise).rejects.toThrow()
+  });
+
+  test('should return a list of students on success', async () => {
+    const { sut } = makeSut()
+    const students = await sut.load(mockLoadAllStudentsParams())
+    expect(students).toEqual(mockStudentModelList())
   });
 });
