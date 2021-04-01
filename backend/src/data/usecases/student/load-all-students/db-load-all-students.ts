01e308278643c68b99cb436f6d61016eeda929e3
Wisinewski <g_gomees@hotmail.com>
2021-04-01 15:36:31 -0300

    feat: ensure DbLoadAllStudents calls LoadAllStudentsRepository with correct values

---
+++ backend/src/data/usecases/student/load-all-students/db-load-all-students.ts
@@ -0,0 +1,14 @@
+import { LoadAllStudentsRepository } from './../../../protocols/db/student/load-all-students-repository';
+import { StudentModel } from './../../../../domain/model/student';
+import { LoadAllStudents, LoadAllStudentsParams } from './../../../../domain/usecases/student/load-all-students';
+
+export class DbLoadAllStudents implements LoadAllStudents {
+  constructor (
+    private readonly loadAllStudentsRepository: LoadAllStudentsRepository
+  ) {}
+
+  async load (data: LoadAllStudentsParams): Promise<StudentModel[]> {
+    await this.loadAllStudentsRepository.loadAllStudents(data)
+    return null
+  }
+}
