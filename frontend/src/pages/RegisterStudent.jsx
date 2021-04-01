722cabac0fec89009812562c7f1341d63d6ff6aa
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-01 16:18:08 -0300

    Add school supply sorting

--- frontend/src/pages/RegisterStudent.jsx
+++ frontend/src/pages/RegisterStudent.jsx
@@ -60,7 +60,17 @@ class RegisterStudent extends Component {
     }
 
     getSupplyCategories() {
-        database.getSchoolSupplyCategories().then(({ categories }) => this.setState({ supplyCategories: categories }));
+        database.getSchoolSupplyCategories().then(({ categories }) => {
+            categories.sort((a,b) => {
+                if(a.name < b.name) {
+                    return -1;
+                }
+                if(a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+             });
+            this.setState({ supplyCategories: categories })});
     }
 
     handleRegister(event) {
