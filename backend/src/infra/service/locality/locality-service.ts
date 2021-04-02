b80e932abca3b9ec6eb11870bd6869eae0cca9bd
João Gutierre <gutierrejoao@gmail.com>
2021-04-01 23:59:19 -0300

    feat: implement loadSubdistrict method

--- backend/src/infra/service/locality/locality-service.ts
+++ backend/src/infra/service/locality/locality-service.ts
@@ -1,8 +1,10 @@
 import { LoadCitiesByUfService } from './../../../data/protocols/service/load-cities-by-uf-service';
 import { LoadUfsService } from './../../../data/protocols/service/load-ufs-service';
 import axios from 'axios'
+import { LoadSubdistrictRepository } from '../../../data/protocols/service/load-subdistrict-repository';
+import { SubdistrictModel } from '../../../domain/model/subdistrict';
 
-export class LocalityService implements LoadUfsService, LoadCitiesByUfService {
+export class LocalityService implements LoadUfsService, LoadCitiesByUfService, LoadSubdistrictRepository {
   async loadAllUfs (): Promise<any> {
     const response = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
     return response.data
@@ -10,6 +12,11 @@ export class LocalityService implements LoadUfsService, LoadCitiesByUfService {
 
   async loadCitiesByUf (uf: string): Promise<any> {
     const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`)
+    return response.data
+  }
+
+  async loadSubdistrict (city: string): Promise<SubdistrictModel[]>{
+    const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/municipios/${city}/subdistritos`)
     return response.data
   }
 }
