1a0e23fae3cff5ea8fc867246990a7e14491cf00
Wisinewski <g_gomees@hotmail.com>
2021-04-02 19:29:47 -0300

    test: ensure DbLoadStudentById throws if LoadStudentByIdRepository throws

--- backend/src/data/usecases/student/load-student-by-id/db-load-student-by-id.spec.ts
+++ backend/src/data/usecases/student/load-student-by-id/db-load-student-by-id.spec.ts
@@ -1,3 +1,4 @@
+import { throwError } from './../../../../domain/test/test-helper';
 import { LoadStudentByIdRepositorySpy } from './../../../test/mock-db-student';
 import { DbLoadStudentById } from './db-load-student-by-id';
 type SutTypes = {
@@ -19,5 +20,12 @@ describe('DbLoadStudentById', () => {
     const { sut, loadStudentByIdRepositorySpy } = makeSut()
     await sut.load('any_id')
     expect(loadStudentByIdRepositorySpy.data).toBe('any_id')
+  });
+
+  test('should throw if LoadStudentByIdRepository throws', async () => {
+    const { sut, loadStudentByIdRepositorySpy } = makeSut()
+    jest.spyOn(loadStudentByIdRepositorySpy, 'loadById').mockImplementationOnce(throwError)
+    const promise = sut.load('any_id')
+    await expect(promise).rejects.toThrow()
   });
 });
