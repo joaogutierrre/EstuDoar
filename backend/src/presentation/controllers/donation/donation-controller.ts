0320d4c71461d1e4d64bf19ca4b0163a8cd30f00
Wisinewski <g_gomees@hotmail.com>
2021-04-03 00:20:10 -0300

    feat: ensure DonationController calls Validation with correct values

--- backend/src/presentation/controllers/donation/donation-controller.ts
+++ backend/src/presentation/controllers/donation/donation-controller.ts
@@ -1,3 +1,4 @@
+import { Validation } from './../../protocols/validation';
 import { InvalidParamError } from './../../errors/invalid-param-error';
 import { serverError, forbidden, ok } from './../../helpers/http/http-helper';
 import { Donate } from './../../../domain/usecases/donation/donate';
@@ -6,11 +7,13 @@ import { Controller } from './../../protocols/controller';
 
 export class DonationController implements Controller {
   constructor (
-    private readonly donate: Donate
+    private readonly donate: Donate,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      await this.validation.validate(httpRequest.body)
       const { accountId } = httpRequest
       const { type, studentId, items } = httpRequest.body
       const donation = await this.donate.donate({
