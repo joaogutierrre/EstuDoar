7e11c34213b3be208608486a0f62f1fe5ca7f667
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-02 23:21:22 -0300

    Change span

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,14 +9,14 @@ class Home extends Component {
             <div className='home'>
                 <Hero />
                 <div className="history-container">
-                    <h2 class="home-title"><span className="txt-circle">Obj</span>etivo EstuDoar</h2>
+                    <h2 class="home-title"><span className="txt-circle">Obje</span>tivo EstuDoar</h2>
                     <div className="history-image-container">
                         <img src={imgObjetivo} alt=""/>
                     </div>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit beatae est quas quasi totam delectus ipsum, omnis, assumenda distinctio doloribus corrupti iste? Excepturi, hic. Aperiam libero recusandae aspernatur eveniet hic.</p>
                 </div>
                 <div className="history-container">
-                    <h2 class="home-title"><span className="txt-circle">Por</span> que doar material é importante?</h2>
+                    <h2 class="home-title"><span className="txt-circle">Por </span>que doar material é importante?</h2>
                     <div className="history-image-container">
                         <img src={imgMotivo} alt=""/>
                     </div>
