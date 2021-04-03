173f3ab2b849f034479024d647b3bc37033fa6f9
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-03 00:48:47 -0300

    Add feed card style

--- frontend/src/components/DonateStudentCard.jsx
+++ frontend/src/components/DonateStudentCard.jsx
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import './DonateStudentCard.css';
-
+import { FaBirthdayCake, FaHome } from "react-icons/fa";
 class DonateStudentCard extends Component {
   render() {
     return (
       <div className="card-container">
-        <div className="image-container">
-          <img
-            src="https://images.unsplash.com/photo-1499323888381-7fd102a793de?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
-            alt="Foto do Estudante"
-          />
+        <div className="grid-two">
+            <div className="image-container">
+              <img
+                src="https://images.unsplash.com/photo-1499323888381-7fd102a793de?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80"
+                alt="Foto do Estudante"
+              />
+            </div>
+              <div className="student-data">
+                <h3>Bruna Souza</h3>
+                <p><FaHome /> São Paulo - SP</p>
+                <p><FaBirthdayCake />10 anos</p>
+              </div>
         </div>
-        <div className="card-content">
-          <div className="student-name">
-            <h3>Bruna Souza</h3>
+          <div className="student-about">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque ea eum minus, obcaecati eius possimus? Quibusdam eveniet</p>
           </div>
-          <div className="list-progress-bar">
-              <p>32% Concluído</p>
-            <progress id="file" value="32" max="100" />
+          <div className="grid-two">
+            <div className="list-progress-bar">
+              <progress id="file" value="32" max="100" />
+            </div>
+            <div className="donate-button">
+              <button>Doar</button>
+            </div>
           </div>
-          <div className="donate-button">
-            <button>Contribuir</button>
-          </div>
-        </div>
       </div>
     );
   }
