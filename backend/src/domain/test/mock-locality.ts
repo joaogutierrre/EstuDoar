3a2831932783c1a7381b715684065161f292992b
João Gutierre <gutierrejoao@gmail.com>
2021-04-02 22:10:57 -0300

    refactor: create mockSubdistrictsModel on mock-locality domain/test

--- backend/src/domain/test/mock-locality.ts
+++ backend/src/domain/test/mock-locality.ts
@@ -1,4 +1,5 @@
 import { UfsModel, CitiesModel } from './../model/locality';
+import { SubdistrictsModel } from './../model/subdistrict'
 
 export const mockUfsModel = (): UfsModel => ({
   ufs: [{
@@ -19,5 +20,17 @@ export const mockCitiesModel = (): CitiesModel => ({
   }, {
     id: 2,
     name: 'São Paulo'
+  }]
+})
+
+export const mockSubdistrictsModel = (): SubdistrictsModel =>({
+  subdistricts:[{
+    id: 'any_id',
+    code: 'any_code',
+    name: 'any_name'
+  },{
+    id: 'other_id',
+    code: 'other_code',
+    name: 'other_name'
   }]
 })
