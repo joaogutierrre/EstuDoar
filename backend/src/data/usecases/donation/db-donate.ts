88fc9bb5777fd0fc417127fca7f6cc399b601916
Wisinewski <g_gomees@hotmail.com>
2021-04-03 14:16:13 -0300

    test: ensure DonationRoutes returns an donation on success

--- backend/src/data/usecases/donation/db-donate.ts
+++ backend/src/data/usecases/donation/db-donate.ts
@@ -13,19 +13,22 @@ export class DbDonate implements Donate {
 
   async donate (data: DonateParams): Promise<DonationModel> {
     const student = await this.loadStudentByIdRepository.loadById(data.studentId)
-    for (const item of student.items) {
-      for (const donatedItem of data.items) {
-        if (item.category === donatedItem.category) {
-          if (item.donated + donatedItem.donated <= item.quantity) {
-            item.donated += donatedItem.donated
-          } else {
-            return null
+    if (student) {
+      for (const item of student.items) {
+        for (const donatedItem of data.items) {
+          if (item.category === donatedItem.category) {
+            if (item.donated + donatedItem.donated <= item.quantity) {
+              item.donated += donatedItem.donated
+            } else {
+              return null
+            }
           }
         }
       }
+      await this.updateStudentByIdRepository.updateById(student)
+      const donation = await this.donateRepository.donate(data)
+      return donation
     }
-    await this.updateStudentByIdRepository.updateById(student)
-    const donation = await this.donateRepository.donate(data)
-    return donation
+    return null
   }
 }
