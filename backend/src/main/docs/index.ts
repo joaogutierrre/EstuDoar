ecdd43ba307e2c1745e133c07363488151aadfa6
Wisinewski <g_gomees@hotmail.com>
2021-04-04 15:33:37 -0300

    docs: add login api docs

--- backend/src/main/docs/index.ts
+++ backend/src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginParamsSchema } from './schemas/login-params-schema';
+import { accountSchema } from './schemas/account-schema';
+import { loginPath } from './paths/login-path'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'EstuDoar TS API',
     description: 'API da aplicação EstuDoar',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
