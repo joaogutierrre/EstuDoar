8501ad9758a2c6978d9d25de356dddeeeb35e18b
João Gutierre <gutierrejoao@gmail.com>
2021-04-05 01:16:03 -0300

    refactor: rename feature and data files

--- backend/src/data/protocols/service/load-subdistrict-repository.ts
+++
@@ -1,5 +0,0 @@
-import { SubdistrictModel } from "../../../domain/model/subdistrict";
-
-export interface LoadSubdistrictRepository {
-    loadSubdistrict: (city: string) => Promise<SubdistrictModel[]>
-}
