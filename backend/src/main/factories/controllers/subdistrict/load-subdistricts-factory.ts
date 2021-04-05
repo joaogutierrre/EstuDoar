d93a891088cf9264fcad09db46b3068cbb94d637
João Gutierre <gutierrejoao@gmail.com>
2021-04-05 02:05:20 -0300

    refactor: rename feature factory files (subdistricts -> schools)

--- backend/src/main/factories/controllers/subdistrict/load-subdistricts-factory.ts
+++
@@ -1,7 +0,0 @@
-import { LoadSubdistrictsController } from "../../../../presentation/controllers/locality/subdistricts/load-subdistricts-controller"
-import { makeApiLoadSubdistricts } from "../../usecases/subdistrict/api-load-subdistricts-factory"
-
-export const makeLoadSubdistrictsController = (): LoadSubdistrictsController => {
-    const loadSubdistrictsController = new LoadSubdistrictsController(makeApiLoadSubdistricts())
-    return loadSubdistrictsController
-  }
--- backend/src/main/factories/usecases/subdistrict/api-load-subdistricts-factory.ts
+++
@@ -1,8 +0,0 @@
-import { ApiLoadSubdistrict } from "../../../../data/usecases/subdistrict/api-load-subdistrict";
-import { LoadSubdistricts } from "../../../../domain/usecases/subdistricts/load-subdistricts";
-import { LocalityService } from "../../../../infra/service/locality/locality-service"
-
-export const makeApiLoadSubdistricts = (): LoadSubdistricts =>{
-    const loadSubdistrictsService = new LocalityService()
-    return new ApiLoadSubdistrict(loadSubdistrictsService)
-} 
