28a8cc7aa03de7b46c6bcdfbcfb0154b9b4bfd02
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-05 15:21:33 -0300

    Add style in dashboard list view

--- frontend/src/pages/StudentList.jsx
+++ frontend/src/pages/StudentList.jsx
@@ -5,11 +5,22 @@ class StudentList extends Component {
         const { location } = this.props;
         const { items } = location.state;
         const listView = items.map(( item, index ) => (
-            <p key={ index }>Item: {item.category} Requisitado: {item.quantity} Doados: {item.donated}</p>
+            <div  key={ index } className="card-container list-card">
+            <li>ITEM: {item.category.toUpperCase()}</li>
+            <li>PEDIDOS: {item.quantity}</li>
+            <li>DOADOS: {item.donated}</li>
+            </div>
         ));
         return(
             <div className="dashboard-my-list">
-                {listView}
+                <div className="f-column">
+                    <h1 className="bg-marker">MINHA LISTA</h1>
+                </div>
+                <div className="list-container">
+                    <ul>
+                        {listView}
+                    </ul>
+                </div>
             </div>
         )
     }
