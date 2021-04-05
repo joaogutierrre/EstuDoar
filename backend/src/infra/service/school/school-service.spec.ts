e5cb292c32b405aec9932d2e4d24c443c5e265de
João Gutierre <gutierrejoao@gmail.com>
2021-04-05 01:49:55 -0300

    feat: implement school services

---
+++ backend/src/infra/service/school/school-service.spec.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { SchoolService } from "./school-service";
+
+type SutTypes = {
+    sut: SchoolService
+  }
+  
+  const makeSut = (): SutTypes => {
+    const sut = new SchoolService()
+    return {
+      sut
+    }
+  }
+
+
+describe('loadSchool', () => {
+    test('should return a list of schools if axios returns status code 200', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(axios, 'get').mockReturnValueOnce(Promise.resolve({
+        data: {
+          statusCode: 200
+        }
+      }))
+      const schools = await sut.loadSchool('city')
+      expect(schools).toBeTruthy()
+    });
+
+    test('should return a list of schools on success', async () => {
+      const { sut } = makeSut()
+      const schools = await sut.loadSchool('3304557')
+      expect(schools).toBeTruthy()
+    });
+  });
